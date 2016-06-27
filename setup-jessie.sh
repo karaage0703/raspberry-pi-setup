a13d5e3e7c2a865abb0f5793395e300fab3cf988
karaage0703 <coldchicken@gmail.com>
2016-06-27 22:25:44 +0900

    add measure time and use function

--- setup-jessie.sh
+++ setup-jessie.sh
@@ -1,37 +1,67 @@
+#!/bin/bash
+
+# update package and firmware
+update_package(){
+	# update package
+	sudo apt-get update
+	sudo apt-get upgrade
+
+	# update firmware
+	sudo rpi-update
+}
+
+# install apt-get package
+apt_get_install(){
+	# setup JAPANESE environment
+	sudo apt-get install -y fonts-vlgothic
+	sudo apt-get install -y ibus-mozc
+
+	sudo apt-get install -y tightvncserver
+
+	# install editor
+	sudo apt-get install -y vim
+	sudo apt-get install -y vim-gtk
+
+	# install gnome-terminal
+	sudo apt-get install -y gnome-terminal
+
+	# install partition setup tool
+	sudo apt-get install -y gparted
+
+	# install virtual terminal
+	sudo apt-get install -y byobu
+}
+
+# setup dotfiles
+setup_dotfiles(){
+	sudo cp ./dotfiles/.bashrc ~/.bashrc
+	sudo cp ./dotfiles/config.txt /boot/config.txt
+	sudo cp ./dotfiles/lxpolkit.desktop /etc/xdg/autostart/lxpolkit.desktop
+
+	sudo cp ./dotfiles/vncboot /etc/init.d/vncboot
+	update-rc.d -f lightdm remove
+	update-rc.d vncboot defaults
+}
+
+START_TIME=`date +%s`
+
+echo "Total Time: ${HH}:${MM}:${SS} (h:m:s)"
+start_time=`date +%s`
+
 # change directory here
 cd `dirname $0`
 
-# update package
-sudo apt-get update
-sudo apt-get upgrade
+update_package
+apt_get_install
+setup_dotfiles
 
-# update firmware
-sudo rpi-update
+END_TIME=`date +%s`
 
-# setup JAPANESE environment
-sudo apt-get install -y fonts-vlgothic
-sudo apt-get install -y ibus-mozc
+SS=`expr ${END_TIME} - ${START_TIME}`
+HH=`expr ${SS} / 3600`
+SS=`expr ${SS} % 3600`
+MM=`expr ${SS} / 60`
+SS=`expr ${SS} % 60`
 
-sudo apt-get install -y tightvncserver
-
-# install editor
-sudo apt-get install -y vim
-sudo apt-get install -y vim-gtk
-
-# install gnome-terminal
-sudo apt-get install -y gnome-terminal
-
-# install partition setup tool
-sudo apt-get install -y gparted
-
-# install virtual terminal
-sudo apt-get install -y byobu
-
-# setup dotfiles
-sudo cp ./dotfiles/.bashrc ~/.bashrc
-sudo cp ./dotfiles/config.txt /boot/config.txt
-sudo cp ./dotfiles/lxpolkit.desktop /etc/xdg/autostart/lxpolkit.desktop
-
-sudo cp ./dotfiles/vncboot /etc/init.d/vncboot
-update-rc.d -f lightdm remove
-update-rc.d vncboot defaults
+echo "Total Time: ${HH}:${MM}:${SS} (h:m:s)"
+ehho "Please reboot"
