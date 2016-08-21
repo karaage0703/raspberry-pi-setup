8cf1069a8b1d24f5285252c966b0137fb8621be7
karaage0703 <coldchicken@gmail.com>
2016-08-21 15:30:55 +0900

    add raspbian jessie lite setup

---
+++ setup-raspbian-jessie-lite.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# update package and firmware
+update_package(){
+	# update package
+	sudo apt-get update
+	sudo apt-get -y upgrade
+
+	# update firmware
+	sudo rpi-update
+}
+
+# install apt-get package
+apt_get_install(){
+	# install editor
+	sudo apt-get install -y vim
+	sudo apt-get install -y vim-gtk
+
+	# install virtual terminal
+	sudo apt-get install -y byobu
+}
+
+# setup dotfiles
+setup_dotfiles(){
+	sudo cp ./dotfiles/keyboard /etc/default/keyboard
+	sudo cp ./dotfiles/config_lite.txt /boot/config.txt
+}
+
+START_TIME=`date +%s`
+
+# change directory here
+cd `dirname $0`
+
+update_package
+apt_get_install
+setup_dotfiles
+
+END_TIME=`date +%s`
+
+SS=`expr ${END_TIME} - ${START_TIME}`
+HH=`expr ${SS} / 3600`
+SS=`expr ${SS} % 3600`
+MM=`expr ${SS} / 60`
+SS=`expr ${SS} % 60`
+
+echo "Total Time: ${HH}:${MM}:${SS} (h:m:s)"
+
+echo "Please reboot"
