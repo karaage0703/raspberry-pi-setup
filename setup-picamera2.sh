3b0d78446080aa0314e884a76983a21ef9b9656a
karaage0703 <coldchicken@gmail.com>
2022-03-18 01:10:47 +0900

    update

--- setup-picamera2.sh
+++ setup-picamera2.sh
@@ -33,6 +33,12 @@ cd
 git clone https://github.com/raspberrypi/picamera2.git
 sh -c "echo \"export PYTHONPATH=/home/pi/picamera2:/home/pi/libcamera/build/src/py:/home/pi/kmsxx/build/py:/home/pi/python-v4l2\" >> ~/.bashrc"
 
+wget -O install_pivariety_pkgs.sh https://github.com/ArduCAM/Arducam-Pivariety-V4L2-Driver/releases/download/install_script/install_pivariety_pkgs.sh
+chmod +x install_pivariety_pkgs.sh
+./install_pivariety_pkgs.sh -p libcamera_dev
+./install_pivariety_pkgs.sh -p libcamera_apps
+./install_pivariety_pkgs.sh -p imx519_kernel_driver
+
 END_TIME=`date +%s`
 
 SS=`expr ${END_TIME} - ${START_TIME}`
